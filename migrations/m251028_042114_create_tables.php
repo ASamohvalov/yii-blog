09209be840ac00a99bfbc3a21979bb599c780d89
artem samohvalov <artym.samohvaloff@gmail.com>
2025-11-04 14:34:32 +0400

    updated migrations

--- migrations/m251028_042114_create_tables.php
+++ migrations/m251028_042114_create_tables.php
@@ -17,7 +17,8 @@ class m251028_042114_create_tables extends Migration
             'salt' => $this->string()->notNull(),
             'email' => $this->string()->notNull(),
             'profile' => $this->string()->notNull(),
-            'auth_key' => $this->string()->notNull()
+            'auth_key' => $this->string()->notNull(),
+            'is_author' => $this->boolean()->notNull()->defaultValue(false)
         ]);
 
         $this->createTable('tbl_post', [
@@ -30,11 +31,13 @@ class m251028_042114_create_tables extends Migration
 
         $this->createTable('tbl_comment', [
             'id' => $this->primaryKey(),
-            'author' => $this->integer()->notNull(),
-            'email' => $this->string()->notNull(),
+            'author_id' => $this->integer()->notNull(),
+            'post_id' => $this->integer()->notNull(),
             'url' => $this->string(),
             'content' => $this->text(),
-            'is_approed' => $this->boolean()->notNull() // status
+            'is_approed' => $this->boolean()->notNull(), // status
+            'FOREIGN KEY (author_id) REFERENCES tbl_user (id)', // $this->addForeignKey not work in sqlite
+            'FOREIGN KEY (post_id) REFERENCES tbl_post (id)'
         ]);
 
         $this->createTable('tbl_tag', [
@@ -50,6 +53,9 @@ class m251028_042114_create_tables extends Migration
             'type' => $this->string()->notNull(),
             'position' => $this->integer()->notNull()
         ]);
+
+        $this->createIndex('idx-comment-author_id', 'tbl_comment', 'author_id');
+        $this->createIndex('idx-comment-post_id', 'tbl_comment', 'post_id');
     }
 
     /**
